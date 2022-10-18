538cf4274bc0c21320e27a7a6f70813a9b6ad2a0
SairilSeb <cyrilseberiaga@gmail.com>
2022-10-18 16:19:31 +0800

    add vue-router

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>
   <v-app>
-    
+    <v-content>
+      <router-view></router-view>
+    </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+import HelloWorld from './components/HelloWorld.vue';
 
 export default {
   name: 'App',
 
   components: {
-    HelloWorld,
+    HelloWorld
   },
 
   data: () => ({
