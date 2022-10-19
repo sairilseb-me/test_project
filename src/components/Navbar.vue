002c30ab47ead42615f96b4a0baa346dd01d567d
SairilSeb <cyrilseberiaga@gmail.com>
2022-10-19 20:09:26 +0800

    navigation drawer tuts

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,6 +1,7 @@
 <template>
     <nav>
        <v-app-bar flat class="grey lighten-4">
+            <v-app-bar-nav-icon @click="triggerDrawer" class="grey--text"></v-app-bar-nav-icon>
             <v-toolbar-title class="text-uppercase grey--text">
                 <span class="font-weight-light">Todo</span>
                 <span>Ninja</span>
@@ -13,6 +14,16 @@
                 </v-icon>
             </v-btn>
        </v-app-bar>
+       <v-navigation-drawer app v-model="drawer" class="grey lighten-3">
+        <div class="d-flex justify-end">
+            <v-btn text @click="triggerDrawer">
+                <v-icon>
+                    mdi-close
+                </v-icon>
+           </v-btn>
+        </div>
+           
+       </v-navigation-drawer>
     </nav>
 </template>
 
@@ -21,7 +32,12 @@
 export default {
     data(){
         return{
-
+            drawer: false,
+        }
+    },
+    methods: {
+        triggerDrawer(){
+            this.drawer = !this.drawer
         }
     }
 }
