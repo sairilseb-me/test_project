c3b26e3cf71951d415245673febf132235c6dba9
SairilSeb <cyrilseberiaga@gmail.com>
2022-10-20 16:17:56 +0800

    grid tuts 2

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,27 +1,61 @@
 <template>
-    <div class="container">
+    <div>
        <v-subheader>
             Dashboard
        </v-subheader>
        <v-divider></v-divider>
        <v-container class="my-5">
-        <v-row>
-          <v-col cols="12" md="6" sm="3">
-            <v-btn block outlined  dark color="purple">1</v-btn>
-          </v-col>
-          <v-col cols="12" md="6" sm="3">
-            <v-btn block outlined >1</v-btn>
-          </v-col>
-          <v-col cols="12" md="6" sm="3">
-            <v-btn block outlined >1</v-btn>
-          </v-col>
-        </v-row>
-        <v-row>
-            <v-col cols="12">
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempora cupiditate, vero, iure laudantium soluta veritatis, libero dolores numquam ab voluptatum necessitatibus ducimus obcaecati. Assumenda sit error, placeat voluptatibus quidem provident.
-                Autem, voluptate ducimus quis laborum minus dicta dolore, doloremque minima dolorem recusandae, corrupti iure explicabo nam. Voluptates maxime cumque ut eius pariatur eos, dignissimos, iste eaque odio adipisci necessitatibus similique.
-            </v-col>
-        </v-row>
+        <v-card flat class="pa-3">
+             <v-card flat class="text-xs-center">
+                <v-row v-for="(data, index) in datas" :key="index">
+                    <v-col cols="12" md="6" sm="12">
+                        <p class="subheading grey--text">Project Title</p>
+                        <p>{{ data.title }}</p>
+                    </v-col>
+                    <v-col cols="6" md="2" sm="2">
+                        <p class="subheading grey--text">Person</p>
+                        <p>{{ data.person }}</p>
+                    </v-col>
+                    <v-col cols="6" md="2" sm="2">
+                        <p class="subheading grey--text">Due Date:</p>
+                        <p>{{ data.due_by }}</p>
+                    </v-col>
+                    <v-col cols="12" md="2" sm="2">
+                        <p class="subheading grey--text">Status</p>
+                        <p>{{ data.status }}</p>
+                    </v-col>
+                </v-row>
+             </v-card>
+        </v-card>
        </v-container>
     </div>
 </template>
+
+<script>
+export default {
+    data() {
+        return {
+            datas: [
+                {
+                    title: 'Create a new website',
+                    person: 'Sairil Seberiaga',
+                    due_by: 'September 14, 2021',
+                    status: 'On-Going'
+                },
+                {
+                    title: 'Check on passport',
+                    person: 'Jermaine Laurie Seberiaga',
+                    due_by: 'March 21, 2022',
+                    status: 'On-Going'
+                },
+                {
+                    title: 'Shout at the children',
+                    person: 'Jerlyn Ramos',
+                    due_by: 'October 21, 2022',
+                    status: 'On-Going'
+                }
+            ],
+        }
+    }
+}
+</script>
--- src/components/Teams.vue
+++ src/components/Teams.vue
@@ -1,11 +1,50 @@
 <template>
-    <div class="container">
+    <div>
         <v-subheader>
             Teams
        </v-subheader>
        <v-divider></v-divider>
        <v-container>
-            <p class="text-sm-left">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Enim reprehenderit eius fuga optio ad pariatur, laboriosam fugit suscipit iure repellendus deserunt distinctio, veritatis excepturi ab autem ipsam dolor doloremque sint!</p>
+           <v-row>
+               <v-col cols="6" md="4" sm="6" v-for="(team, index) in teams" :key="index">
+                    <v-card flat class="text-center pa-3">
+                        <v-card-title><span class="text-center">Name: {{ team.name }}</span></v-card-title>
+                        <v-divider></v-divider>
+                        <v-card-subtitle>Role: {{ team.role }}</v-card-subtitle>
+                    </v-card>
+               </v-col>
+           </v-row>
        </v-container>
     </div>
 </template>
+
+<script>
+export default {
+   data(){
+    return {
+        teams: [
+            {
+                name: "Sairil Seberiaga",
+                role: "Web Developer"
+            },
+            {
+                name: "Jermaine Laurie Seberiaga",
+                role: "Graphics Designer"
+            },
+            {
+                name: "Jeryn Ramos",
+                role: "Atchay"
+            },
+            {
+                name: "Daniel Gepolongca",
+                role: "Manug Sabad"
+            },
+            {
+                name: "Jay Ann Morada",
+                role: "Muse"
+            }
+        ]
+    }
+   }
+}
+</script>
