47694d55e219964b9da29183769a345fc2315165
SairilSeb <cyrilseberiaga@gmail.com>
2022-10-20 13:36:14 +0800

    grid tuts 1

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -6,11 +6,20 @@
        <v-divider></v-divider>
        <v-container class="my-5">
         <v-row>
-            <v-col cols="12" md="6">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore deserunt dolorum, ullam adipisci, iusto doloribus illum repellat earum consequuntur rem assumenda nulla et dicta? Debitis quas nesciunt quae alias pariatur.
-            </v-col>
-            <v-col cols="12" md="6">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore in quisquam totam eum quaerat odit, corporis reiciendis saepe ducimus accusantium fugit architecto neque deleniti rerum laboriosam? Quam officiis cumque dolore!
+          <v-col cols="12" md="6" sm="3">
+            <v-btn block outlined  dark color="purple">1</v-btn>
+          </v-col>
+          <v-col cols="12" md="6" sm="3">
+            <v-btn block outlined >1</v-btn>
+          </v-col>
+          <v-col cols="12" md="6" sm="3">
+            <v-btn block outlined >1</v-btn>
+          </v-col>
+        </v-row>
+        <v-row>
+            <v-col cols="12">
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempora cupiditate, vero, iure laudantium soluta veritatis, libero dolores numquam ab voluptatum necessitatibus ducimus obcaecati. Assumenda sit error, placeat voluptatibus quidem provident.
+                Autem, voluptate ducimus quis laborum minus dicta dolore, doloremque minima dolorem recusandae, corrupti iure explicabo nam. Voluptates maxime cumque ut eius pariatur eos, dignissimos, iste eaque odio adipisci necessitatibus similique.
             </v-col>
         </v-row>
        </v-container>
